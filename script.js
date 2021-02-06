a2b8ef41e426485fb9312a47e248360694d1ab23
XuMuKxD <shabmish04@yandex.ru>
2021-02-06 09:19:29 +0300

    Third commit

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
 $(document).ready(function() {
 
-    $("#add").click(function(){
-        console.log(1);
+    var arrayAds = new Array();
+
+    $("#add").click(function() {
+        
+        $("#modal").show();
     });
+    
+    $("#close").click(function() {
+        
+        $("#modal").hide();
+    });
+
+
+    
+    
+
+    
+    $("#new").click(function() {
+        var ad = {
+            adText: $("input[name='text']").val(),
+            contactName: $("input[name='name']").val(),
+            contactPhone: $("input[name='phone']").val()
+           
+        }
+        
+        arrayAds.push(ad)
+        
+        console.log(arrayAds)
+
+        renderAds();
+
+        $("#modal").hide();
+    });
+
+    function renderAds(){
+        $("#content").html('');
+
+        arrayAds.map(ad => {
+            var adHTML = 
+            '<div class="Ads__box">'+
+                '<div class="Box__info">'+ ad.adText +'</div>'+
+                '<div class="Box__name">'+ ad.contactName +'</div>'+
+                '<div class="Box_phone">'+ ad.contactPhone +'</div>'+
+            '</div>';
+            
+            $("#content").append(adHTML);
+        });
+
+      
+    }
 });
